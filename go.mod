3afe5cd4a61f80daddde23546dbb56f3f0036298
Liza <86650306+6uf@users.noreply.github.com>
2022-10-28 18:29:13 -0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/6uf/Secure
+
+go 1.19
+
+require (
+	github.com/6uf/Encrypt v0.0.0-20221028212650-4e000a39eda9 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/jaypipes/ghw v0.9.0 // indirect
+	github.com/jaypipes/pcidb v1.0.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	howett.net/plist v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package Secure
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/6uf/Encrypt"
+	"github.com/jaypipes/ghw"
+)
+
+func EncryptData() (string, error) {
+	bios, err := ghw.BIOS()
+	if err != nil {
+		return "", err
+	}
+	gpu, err := ghw.GPU()
+	if err != nil {
+		return "", err
+	}
+	var Data string
+	if len(gpu.GraphicsCards) > 0 {
+		Data = "-" + gpu.GraphicsCards[0].DeviceInfo.Product.ID
+	}
+	baseboard, err := ghw.Baseboard()
+	if err != nil {
+		return "", err
+	}
+	product, err := ghw.Product()
+	if err != nil {
+		return "", err
+	}
+	return Encrypt.Encode(fmt.Sprintf("\n\n %v-%v \n\n %v-%v-%v-%v \n\n %v-%v%v \n |k__wdmmWOowkdflPP{wdkWSWS23FD|",
+		bios.Vendor, baseboard.Vendor,
+		baseboard.SerialNumber, product.SerialNumber, product.UUID, product.Name,
+		baseboard.Product, Encrypt.Encode(baseboard.SerialNumber, strings.Replace(product.UUID, "-", "", -1)), Data), "c0ef529e570411ed9b6a0242ac120002"), nil
+}
